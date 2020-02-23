623617269dc8aa9583313ca1f951167091325c4e
Nifemi <deyeminifemi@gmail.com>
2020-02-23 18:56:52 +0100

    Added fonts default styling and react router config

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
+import AllProducts from "./pages/AllProducts";
 
 function App() {
-  return (
-    <>
-
-    </>
-  );
+    return (
+        <Router>
+            <Switch>
+                <Route path={'/operations/production-house'} component={AllProducts}/>
+                <Route component={AllProducts}/>
+            </Switch>
+        </Router>
+    );
 }
 
 export default App;
